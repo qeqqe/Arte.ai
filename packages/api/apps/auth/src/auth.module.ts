0d011f107e09899e6b9007023d44cf505a2ebc0d
Abhinab Singh <168533332+qeqqe@users.noreply.github.com>
2025-02-20 22:15:10 +0530

    feat(client): add SolidJS client and its docker integration
    - Updated pnpm-workflow for better monorepo integration.
    
    - Updated dockerfiles for hot reloads in watch mode.
    
    - Added Pino for better logging.

--- packages/api/apps/auth/src/auth.module.ts
+++ packages/api/apps/auth/src/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [LoggerModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
