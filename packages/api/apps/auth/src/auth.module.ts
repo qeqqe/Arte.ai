1bd6c7af0911b4307f72b6e2b003b83a89a4ece1
Abhinab Singh <168533332+qeqqe@users.noreply.github.com>
2025-02-21 00:20:59 +0530

    feat(auth): added auth starter microservice
    - Added drizzle module.
    
    - basic user schema.

--- packages/api/apps/auth/src/auth.module.ts
+++ packages/api/apps/auth/src/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- packages/api/apps/auth/src/main.ts
+++ packages/api/apps/auth/src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.use(cookieParser());
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: configService.get('PORT'),
+    },
+  });
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3001);
+  await app.startAllMicroservices();
 }
 bootstrap();
