6fbb469ff4c965e5b7f9ff9d025a05aadff35d76
Abhinab Singh <168533332+qeqqe@users.noreply.github.com>
2025-02-20 23:26:09 +0530

    feat(dev): This is the dev brach where all the api and client code will be pushed

---
+++ packages/api/drizzle.config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './libs/common/**/*.schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
