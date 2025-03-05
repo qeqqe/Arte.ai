f9ba88eafa61a311b066ab6f9d78860167e72191
qeqqe <armansingh3172006@gmail.com>
2025-03-05 21:54:16 +0530

    feat(github): Fix user repo not getting stored in database
    - Edit user's github top repo's schema

--- packages/api/libs/common/src/github/github-repo.schema.ts
+++ packages/api/libs/common/src/github/github-repo.schema.ts
@@ -1,5 +1,12 @@
 import { InferInsertModel, InferSelectModel, relations } from 'drizzle-orm';
-import { integer, jsonb, pgTable, text, uuid } from 'drizzle-orm/pg-core';
+import {
+  integer,
+  jsonb,
+  pgTable,
+  text,
+  uuid,
+  timestamp,
+} from 'drizzle-orm/pg-core';
 import { users } from '../user';
 
 export const userPinnedRepo = pgTable('user_pinned_repo', {
@@ -9,12 +16,13 @@ export const userPinnedRepo = pgTable('user_pinned_repo', {
   description: text('description').notNull().default('No description'),
   stargazerCount: integer('stargazer_count').notNull().default(0),
   forkCount: integer('fork_count').notNull().default(0),
+  userId: uuid('user_id').references(() => users.id),
   primaryLanguage: text('primary_language').notNull().default('Unknown'),
   repositoryTopics: jsonb('repository_topics').notNull().default('[]'),
   languages: jsonb('languages').notNull().default('[]'),
   readme: text('readme').notNull().default('No readme.md exists'),
-  createdAt: integer('created_at').notNull().default(Date.now()),
-  updatedAt: integer('updated_at').notNull().default(Date.now()),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
 
 export type PinnedRepo = InferSelectModel<typeof userPinnedRepo>;
@@ -24,7 +32,7 @@ export const userPinnedRepoToUserRelation = relations(
   userPinnedRepo,
   ({ one }) => ({
     user: one(users, {
-      fields: [userPinnedRepo.id],
+      fields: [userPinnedRepo.userId],
       references: [users.id],
     }),
   }),
